f4f73e9f12cc3aade196d0d8157721b1a3948c3e
Hugo Matinho <hugo.matinho@ignidata.com>
2015-05-25 21:31:22 +0100

    updated sha256

--- gradle.rb
+++ gradle.rb
@@ -1,7 +1,6 @@
 class Gradle < Formula
   homepage "https://www.gradle.org/"
   url "https://services.gradle.org/distributions-snapshots/gradle-2.5-20150523220017+0000-bin.zip"
-  sha256 "d0a97ca6c029134914a8ea30300c264069bbd865b0918f0ce309be5b5871ebfb"
 
   def install
     libexec.install %w[bin lib]
