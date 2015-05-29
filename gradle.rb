542275543a9a987734f53e9d7d30e8cf4847a419
Hugo Matinho <hugo.matinho@ignidata.com>
2015-05-29 17:46:16 +0100

    updated nightly

--- gradle.rb
+++ gradle.rb
@@ -1,6 +1,6 @@
 class Gradle < Formula
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions-snapshots/gradle-2.5-20150527220016+0000-bin.zip"
+  url "https://services.gradle.org/distributions-snapshots/gradle-2.6-20150528220039+0000-bin.zip"
 
   def install
     libexec.install %w[bin lib]
